0c5bcb80d3c098b9995e26742bf6c0d04ba58d1b
Animesh Kumar <kumaranimesh923@gmail.com>
2025-02-10 11:53:03 +0530

    fix: add files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
-const pro = null;
-vwe;
+const dropdownBtn = document.getElementById("btn");
+const dropdownMenu = document.getElementById("dropdown");
+const toggleArrow = document.getElementById("arrow");
 
-function name(param, p, p2, p3) {
-  
-}
+// Toggle dropdown function
+const toggleDropdown = function () {
+  dropdownMenu.classList.toggle("show");
+  toggleArrow.classList.toggle("arrow");
+};
+
+// Toggle dropdown open/close when dropdown button is clicked
+dropdownBtn.addEventListener("click", function (e) {
+  e.stopPropagation();
+  toggleDropdown();
+});
+
+// Close dropdown when dom element is clicked
+document.documentElement.addEventListener("click", function () {
+  if (dropdownMenu.classList.contains("show")) {
+    toggleDropdown();
+  }
+});
